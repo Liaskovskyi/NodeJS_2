f0affaf7a8ebb486757e792a270f47e29a0af094
Liaskovskyi <105741940+Liaskovskyi@users.noreply.github.com>
2023-12-30 17:19:45 +0200

    vercel

--- src/server.js
+++ src/server.js
@@ -2,54 +2,57 @@ import * as http from 'node:http';
 import router from './router.js';
 import defaultHandler from './defaultHandler.js';
 import helpers from './helpers.js';
-import { safeJSON, parseXML} from './utils.js';
+import {safeJSON} from './utils.js';
 
-const processedContentTypes = {
-    'text/html': (text) =>  callback(null, text),
-    'text/plain': (text, callback) => callback(null, text),
-    'application/json': (json, callback) => callback(null, safeJSON(json, {})),
-    'application/x-www-form-urlencoded': (data, callback) =>
-      callback(null, Object.fromEntries(new URLSearchParams(data))),
-    'application/xml': (xml, callback) => parseXML(xml, callback),  
-  };
+const proccessedContentTypes = {
+    'text/html': (text) => text,
+    'text/plain': (text) => text,
+    'application/json': (json) => safeJSON(json, {}),
+    'application/x-www-form-urlencode': (data) => {
+        return Object.fromEntries(new URLSearchParams(data))
+    }
+};
 
-  export function createHttpServer( port = 9000) {
-  const server = http.createServer(async (req, res) => {
+export function createHttpServer( port = 9000) {
+const server = http.createServer(async (req, res) => {
     const url = new URL(req.url || '/', `https://${req.headers.host}`);
     const routeModule = router.get(url.pathname) ?? {};
     const handler = routeModule[req?.method] ?? defaultHandler;
+
     let payload = {};
     let rawRequest = '';
     for await (const chunk of req) {
-      rawRequest += chunk;
+        rawRequest += chunk;
     }
-    if (req.headers['content-type']) {
-      const contentType = req.headers['content-type'].split(';')[0];
-      if (processedContentTypes[contentType]) {
-        processedContentTypes[contentType](rawRequest, (fallback, result) => {           
-            payload = result || fallback;
-          
-        });
-      }
+
+    if (req.headers['Content-type']) {
+        const contentType = req.headers['Content-type'].split(';')[0];
+        if (proccessedContentTypes[contentType]) {
+            payload = proccessedContentTypes[contentType](rawRequest);
+        }
     }
+
     try {
         handler(req, Object.assign(res, helpers), url, payload, rawRequest);
-      } catch (err) {
+    } catch (e) {
         res.statusCode = 500;
-        res.end('internal error');
-      }
-  });
-server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+        res.end(e);
+    }
 }).listen(port, () => console.log('Server started on port: ', port));
-  
+
+
+server.on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+
 
 process.on('SIGINT', () => {
-  server.close((error) => {
-    if (error) {
-      console.error(error);
-      process.exit(1);
-    }
-  });
+    server.close((error) => {
+        if (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    });
 });
-  }
+}

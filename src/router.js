3387eb2d0b4774581de8986e52a5f4f2ee35d8f1
Liaskovskyi <105741940+Liaskovskyi@users.noreply.github.com>
2023-12-30 18:11:27 +0200

    vercel

--- src/router.js
+++ src/router.js
@@ -13,15 +13,19 @@ async function loadRoutesDir(dirname, base) {
   const workdir = path.join(baseDir, relativePath);
 
   try {
+    console.log("trying")
     const dir = await readdir(workdir, { withFileTypes: true });
     for (const dirent of dir) {
+      console.log("dirent.isFile(): ",dirent.isFile(), "path.extname(dirent.name): ",path.extname(dirent.name), "path.basename(dirent.name,'.js')",path.basename(dirent.name,'.js'))
       if (dirent.isDirectory()) {
+        console.log("isDirectory - true")
         await loadRoutesDir(dirent.name, path.join(base, dirname));
       } else if (
         dirent.isFile() &&
         path.extname(dirent.name) === '.js' &&
         path.basename(dirent.name, '.js') === 'index'
       ) {
+        console.log("index.js = true")
         let modulePath = pathToFileURL(path.join(workdir, dirent.name));
         let module = await import(modulePath);
         console.log("relativepath: ",relativePath, "module: ", module)
